e003f39723cd7d1905bddd1cb93a4c3621ba52b9
hjs6877 <hjs6877@gmail.com>
2015-05-04 22:50:26 +0900

    마이바티스 설정 및 테스트 완료

--- backend/src/main/java/kr/co/siksco/backend/controller/LoginController.java
+++ backend/src/main/java/kr/co/siksco/backend/controller/LoginController.java
@@ -1,13 +1,16 @@
 package kr.co.siksco.backend.controller;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import kr.co.siksco.backend.controller.LoginController;
+import kr.co.siksco.backend.service.UserService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 	public static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(HttpServletRequest request, Map<String, Object> model){
@@ -25,7 +31,13 @@ public class LoginController {
 		logger.info("userEmail: " + userEmail);
 		logger.info("userPass: " + userPass);
 		
-		
+		logger.info("서비스 객체: " + userService);
+		try {
+			logger.info("Dao 호출 테스트: " + userService.getUser());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
