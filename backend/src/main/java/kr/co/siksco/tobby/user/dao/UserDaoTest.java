6fd77f84e3cde6594f801ebf271aef001109e35d
hjs6877 <hjs6877@gmail.com>
2015-05-09 12:59:41 +0900

    g

--- backend/src/main/java/kr/co/siksco/tobby/user/dao/UserDaoTest.java
+++ backend/src/main/java/kr/co/siksco/tobby/user/dao/UserDaoTest.java
@@ -2,12 +2,16 @@ package kr.co.siksco.tobby.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import kr.co.siksco.tobby.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao dao = new UserDao(connectionMaker);
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("hjs6877");
